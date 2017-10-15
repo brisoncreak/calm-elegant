58293d9605ffc9bb16b60aa7f5c9338bfff420b0
creakcc <creakcc@gmail.com>
2017-10-15 17:39:48 +0800

    auto-reply shuzhi3lian

--- weixinhao.py
+++ weixinhao.py
@@ -1,11 +1,63 @@
 from flask import Flask
+from flask import request
+from time import time
+import hashlib
+import templet
+
+import xml.etree.ElementTree as et
+
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '123'
+
+@app.route('/weixin',methods=['GET','POST'])
+def index():
+    if request.method == 'GET':
+        my_signature = request.args.get('signature')
+        my_timestamp = request.args.get('timestamp')
+        my_nonce = request.args.get('nonce')
+        my_echostr = request.args.get('echostr')
+
+        token = 'creak'
+
+        data = [token,my_timestamp,my_nonce]
+        data.sort()
+
+        temp = ''.join(data)
+
+        mysignature = hashlib.sha1(temp).hexdigest()
+
+        if my_signature ==mysignature:
+            return my_echostr
+        else:
+            return ''
+
+    if request.method == 'POST':
+        xml_data = request.stream.read()
+        xml_rec = et.fromstring(xml_data)
+
+        ToUserName = xml_rec.find('ToUserName').text
+        fromUser = xml_rec.find('FromUserName').text
+        MsgType = xml_rec.find('MsgType').text
+        Content = xml_rec.find('Content').text
+        MsgId = xml_rec.find('MsgId').text
+
+        a = random.randint(1,3)
+        if a==1:
+            Content = 'cnm'
+        elif a==2:
+            Content = 'nmb'
+        else:
+            Content = 'ngsb'
+            
+
+        return templet.reply_templet(MsgType) % (fromUser, ToUserName, str(int(time())), Content)
+
 
 
 if __name__ == '__main__':
